336fd201ae7aff722e13034f1b860683842a9da8
Shufan Wang <shufan.wang@epfl.ch>
2022-09-27 13:34:24 +0200

    update state transition preCheck

--- go-ethereum-1.10.23/core/state_transition.go
+++ go-ethereum-1.10.23/core/state_transition.go
@@ -222,15 +222,26 @@ func (st *StateTransition) preCheck() error { //@remind nonce correction
 		// Make sure this transaction's nonce is correct.
 		stNonce := st.state.GetNonce(st.msg.From())
 
-		if msgNonce := st.msg.Nonce(); stNonce < msgNonce {
-			return fmt.Errorf("%w: address %v, tx: %d state: %d", ErrNonceTooHigh,
-				st.msg.From().Hex(), msgNonce, stNonce)
-		} else if stNonce > msgNonce {
-			return fmt.Errorf("%w: address %v, tx: %d state: %d", ErrNonceTooLow,
-				st.msg.From().Hex(), msgNonce, stNonce)
-		} else if stNonce+1 < stNonce {
-			return fmt.Errorf("%w: address %v, nonce: %d", ErrNonceMax,
-				st.msg.From().Hex(), stNonce)
+		if st.msg.Type() == types.EncryptedTxType {
+			//@remind currently simply require the nonce is older than current nonce, so it won't be executed when the order is set, but executed upon decryption
+			if msgNonce := st.msg.Nonce(); stNonce == msgNonce {
+				return fmt.Errorf("%w: address %v, tx: %d state: %d, ordering the encrypted tx", ErrNonceTooHigh,
+					st.msg.From().Hex(), msgNonce, stNonce)
+			} else if stNonce < msgNonce {
+				return fmt.Errorf("%w: address %v, tx: %d state: %d", ErrNonceTooHigh,
+					st.msg.From().Hex(), msgNonce, stNonce)
+			}
+		} else {
+			if msgNonce := st.msg.Nonce(); stNonce < msgNonce {
+				return fmt.Errorf("%w: address %v, tx: %d state: %d", ErrNonceTooHigh,
+					st.msg.From().Hex(), msgNonce, stNonce)
+			} else if stNonce > msgNonce {
+				return fmt.Errorf("%w: address %v, tx: %d state: %d", ErrNonceTooLow,
+					st.msg.From().Hex(), msgNonce, stNonce)
+			} else if stNonce+1 < stNonce {
+				return fmt.Errorf("%w: address %v, nonce: %d", ErrNonceMax,
+					st.msg.From().Hex(), stNonce)
+			}
 		}
 		// Make sure the sender is an EOA
 		if codeHash := st.state.GetCodeHash(st.msg.From()); codeHash != emptyCodeHash && codeHash != (common.Hash{}) {

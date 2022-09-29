710aa8bbb1658c449a04a03d636fae77f6b68d33
Shufan Wang <shufan.wang@epfl.ch>
2022-09-29 10:44:31 +0200

    remove some log

--- go-ethereum-1.10.23/core/state_transition.go
+++ go-ethereum-1.10.23/core/state_transition.go
@@ -182,7 +182,6 @@ func NewStateTransition(evm *vm.EVM, msg Message, gp *GasPool) *StateTransition 
 // indicates a core error meaning that the message would always fail for that particular
 // state and would never be accepted within a block.
 func ApplyMessage(evm *vm.EVM, msg Message, gp *GasPool) (*ExecutionResult, error) {
-	log.Info("$ enter ApplyMessage")
 	return NewStateTransition(evm, msg, gp).TransitionDb()
 }
 
@@ -294,7 +293,6 @@ func (st *StateTransition) preCheck() error { //@remind nonce correction
 // However if any consensus issue encountered, return the error directly with
 // nil evm execution result.
 func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
-	log.Info("$ enter TransitionDb")
 	// First check this message satisfies all consensus rules before
 	// applying the message. The rules include these clauses
 	//

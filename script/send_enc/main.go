fe69bc0152376a907443f7a139a397bb9fae3c53
Julie Bettens <julie.bettens@epfl.ch>
2024-02-27 15:18:36 +0100

    add send_enc script

---
+++ script/send_enc/main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"context"
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"log"
+	"math/big"
+	"path"
+
+	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/f3b"
+)
+
+func sendEtherF3bEnc(client *ethclient.Client, ks *keystore.KeyStore, from accounts.Account, to common.Address, val *big.Int, gasLimit uint64, calldata []byte) (error) {
+	nonce, err := client.PendingNonceAt(context.Background(), from.Address); 
+	if err != nil {
+		return err
+	}
+
+	// get gas price
+	gasPrice, err := client.SuggestGasPrice(context.Background()); 
+	if err != nil {
+		return err
+	}
+	// get chainID
+	chainID, err := client.ChainID(context.Background()); 
+	if err != nil {
+		return err
+	}
+
+	dkgcli := f3b.NewDkgCli()
+
+	label := binary.BigEndian.AppendUint64(from.Address.Bytes(), nonce)
+	plaintext := append(to.Bytes(), calldata...)
+
+	ciphertext, err := dkgcli.Encrypt(label, plaintext)
+	if err != nil {
+		return err
+	}
+
+	enc := &types.EncryptedTx{
+		ChainID:    chainID,
+		Nonce:      nonce,
+		GasFeeCap:  gasPrice,
+		GasTipCap:  big.NewInt(10),
+		Gas:        gasLimit,
+		Value:      val,
+		Payload:    ciphertext,
+	}
+	tx := types.NewTx(enc)
+
+	// sign
+	signedTx, err := ks.SignTx(from, tx, chainID);
+	if err != nil {
+		return err
+	}
+
+	if err = client.SendTransaction(context.Background(), signedTx); err != nil {
+		return err
+	}
+
+	fmt.Println(signedTx.Hash().Hex())
+	return nil
+}
+
+func usage() {
+	fmt.Println("Usage: send_enc [options] <to> [calldata]")
+	fmt.Println("issue an encrypted transaction to the given address with optional calldata")
+	flag.PrintDefaults()
+}
+
+func main() {
+	if err := main2(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main2() error {
+	var to common.Address
+	var calldata []byte
+
+	gethDir := flag.String("ethdir", ".ethereum/", "geth client directory")
+	var value *big.Int
+	flag.Func("value", "call value in wei", func(s string) error {
+		var ok bool
+		if 
+		value, ok = new(big.Int).SetString(s, 10);
+		!ok {
+			return fmt.Errorf("invalid value: %s", s)
+		}
+		return nil
+	})
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		usage()
+		return fmt.Errorf("missing receiver address")
+	}
+	if arg := flag.Arg(0); common.IsHexAddress(arg) {
+		to = common.HexToAddress(arg)
+	}
+	// optional, defaults to ""
+	calldata = common.FromHex(flag.Arg(1))
+
+	// unlock the pre fund user account
+	// FIXME: hardcoded
+	ks := keystore.NewKeyStore(path.Join(*gethDir, "keystore"), keystore.StandardScryptN, keystore.StandardScryptP)
+	from := ks.Accounts()[1]
+	if err := ks.Unlock(from, ""); err != nil {
+		return err
+	}
+
+	client, err := ethclient.Dial(path.Join(*gethDir, "geth.ipc")); 
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	gasLimit := uint64(1000000) // FIXME: hardcoded
+
+	return sendEtherF3bEnc(client, ks, from, to, value, gasLimit, calldata)
+}

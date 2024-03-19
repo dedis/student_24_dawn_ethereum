33e86edd2084a45cd7b2a9d4aaad8dcfaa8b4c9a
Julie Bettens <julie.bettens@epfl.ch>
2024-03-19 09:21:36 +0100

    Revert "add HMAC"
    This reverts commit 868de3cd65a53b2583bd962ff8d2d18f48f4f39d.

--- dela/dkg/pedersen_bn256/ibe/ibe.go
+++ dela/dkg/pedersen_bn256/ibe/ibe.go
@@ -6,12 +6,9 @@ import (
 	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
-	"crypto/hmac"
-	"crypto/sha256"
 	"crypto/sha512"
 	"errors"
 	"fmt"
-	"hash"
 
 	kyber "go.dedis.ch/kyber/v3"
 	"go.dedis.ch/kyber/v3/pairing"
@@ -20,11 +17,8 @@ import (
 	"golang.org/x/crypto/hkdf"
 )
 
-const tagSize = 32
-
 // Based on https://github.com/drand/kyber/blob/master/encrypt/ibe/ibe.go
 // with G1 and G2 flipped and AES-CTR instead of Blake2s
-// and appended HMAC-SHA256 tag
 
 type CiphertextCPA struct {
 	U kyber.Point
@@ -42,14 +36,11 @@ func (ct *CiphertextCPA) Serialize(suite pairing.Suite) ([]byte, error) {
 		return nil, fmt.Errorf("unexpected point marshalled size: %v", len(marshalledU))
 	}
 	buf := make([]byte, 0, pointMarshalledSize+len(ct.V))
-	buf = append(buf, marshalledU...)
-	buf = append(buf, ct.V...)
+	buf =  append(buf, marshalledU...)
+	buf =  append(buf, ct.V...)
 	return buf, nil
 }
 func (ct *CiphertextCPA) Deserialize(suite pairing.Suite, data []byte) error {
-	if len(data) < pointMarshalledSize+tagSize {
-		return errors.New("data too short")
-	}
 	marshalledU := data[:pointMarshalledSize]
 	U := suite.G2().Point()
 	U.UnmarshalBinary(marshalledU)
@@ -62,33 +53,20 @@ type hashablePoint interface {
 	Hash([]byte) kyber.Point
 }
 
-type sessionKeys struct {
-	encKey [32]byte
-	macKey [32]byte
-}
-
-func (keys *sessionKeys) fromGt(GidT kyber.Point) error {
+func gtToStream(GidT kyber.Point) (cipher.Stream, error) {
 	seed, err := GidT.MarshalBinary()
 	if err != nil {
-		return err
+		return nil, err
 	}
+	key := make([]byte, 32)
 	kdf := hkdf.New(sha512.New, seed, nil, nil)
-	kdf.Read(keys.encKey[:])
-	kdf.Read(keys.macKey[:])
-	return nil
-}
-
-func (keys *sessionKeys) keyStream() (cipher.Stream, error) {
-	block, err := aes.NewCipher(keys.encKey[:])
+	kdf.Read(key)
+	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
 	iv := make([]byte, 16) //NOTE: this is allowed because the key is used only once
 	return cipher.NewCTR(block, iv), nil
-}
-
-func (keys *sessionKeys) mac() (hash.Hash, error) {
-	return hmac.New(sha256.New, keys.macKey[:]), nil
 }
 
 func DeriveEncryptionKeyOnG2(suite pairing.Suite, X kyber.Point, label []byte) (kyber.Point, error) {
@@ -101,47 +79,24 @@ func DeriveEncryptionKeyOnG2(suite pairing.Suite, X kyber.Point, label []byte) (
 }
 
 func EncryptCPAonG2(suite pairing.Suite, P kyber.Point, msg []byte) (*CiphertextCPA, error) {
-	var keys sessionKeys
 	r := suite.G2().Scalar().Pick(random.New())
 	U := suite.G2().Point().Mul(r, suite.G2().Point().Base())
-	err := keys.fromGt(suite.GT().Point().Mul(r, P))
+	xof, err := gtToStream(suite.GT().Point().Mul(r, P))
 	if err != nil {
 		return nil, err
 	}
-	xof, err := keys.keyStream()
-	if err != nil {
-		return nil, err
-	}
-	V := make([]byte, len(msg), len(msg)+tagSize)
+	V := make([]byte, len(msg))
 	xof.XORKeyStream(V, msg)
-
-	mac, err := keys.mac()
-	mac.Write(V)
-	V = mac.Sum(V)
 
 	return &CiphertextCPA{U, V}, nil
 }
 
 func DecryptCPAonG2(suite pairing.Suite, decKey kyber.Point, ct *CiphertextCPA) ([]byte, error) {
-	var keys sessionKeys
-	err := keys.fromGt(suite.Pair(decKey, ct.U))
+	xof, err := gtToStream(suite.Pair(decKey, ct.U))
 	if err != nil {
 		return nil, err
 	}
-
-	mac, err := keys.mac()
-	mac.Write(ct.V[:len(ct.V)-tagSize])
-	tag := mac.Sum(nil)
-	if !hmac.Equal(tag, ct.V[len(ct.V)-tagSize:]) {
-		return nil, errors.New("MAC mismatch")
-	}
-
-	xof, err := keys.keyStream()
-	if err != nil {
-		return nil, err
-	}
-	msg := make([]byte, len(ct.V[:len(ct.V)-tagSize]))
-	xof.XORKeyStream(msg, ct.V[:len(ct.V)-tagSize])
-
+	msg := make([]byte, len(ct.V))
+	xof.XORKeyStream(msg, ct.V)
 	return msg, nil
 }

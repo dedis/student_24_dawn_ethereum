c1d71919e170a00d76af10dd82f5c9e32acec459
Julie Bettens <julie.bettens@epfl.ch>
2024-04-17 11:20:28 +0200

    s/verbosely/visibly/g

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ export ETH_KEYSTORE="$(pwd)/keystore"
 touch $tempdir/password
 export ETH_PASSWORD="$tempdir/password"
 
-verbosely() {
+visibly() {
 	echo
 	echo $'\e[1m$' "$*"$'\e[0m'
 	eval "$@"
@@ -29,13 +29,13 @@ producer_datadir=$tempdir/producer
 producer_nodekey="e74976d3e1d9069b85d6659038105fe601696a0ddcb63f0407b11328e341a47c"
 producer_addr="enode://3d1bb945ae2e250f5fe23f6da3f150b1af4d425bd280bdbfc3e7626ae4625cac2cfb3a59469b67528765a50237c0f434bc3cebcb63118b21949e4139de6b9fb1@127.0.0.1:30303"
 
-verbosely 'geth -datadir "$producer_datadir" init clique.json'
+visibly 'geth -datadir "$producer_datadir" init clique.json'
 
 cp keystore/$coinbase $producer_datadir/keystore
 tmux neww -d geth -datadir "$producer_datadir" -nodiscover -mine -password /dev/null -unlock $coinbase -nodekeyhex $producer_nodekey -nat none
 
 observer_datadir=$tempdir/observer
-verbosely 'geth -datadir "$observer_datadir" init clique.json'
+visibly 'geth -datadir "$observer_datadir" init clique.json'
 tmux neww -d geth -datadir "$observer_datadir" -http -port 0 -authrpc.port 0 -bootnodes $producer_addr -verbosity 4
 export ETH_RPC_URL=http://localhost:8545
 
@@ -45,21 +45,21 @@ while ! cast block-number 2> /dev/null; do
 done
 
 (cd contracts
-	verbosely 'forge create --keystore "$ETH_KEYSTORE/$deployer" --from $deployer Auction'
+	visibly 'forge create --keystore "$ETH_KEYSTORE/$deployer" --from $deployer Auction'
 )
 
 auction_contract=0xef434c1405f66997CBf4a04FDDed518C28a6a013
 
-verbosely 'cast send --async --keystore $ETH_KEYSTORE/$deployer --from $deployer $auction_contract "start()"'
+visibly 'cast send --async --keystore $ETH_KEYSTORE/$deployer --from $deployer $auction_contract "start()"'
 
 # send an encrypted bid
-verbosely 'go run ./script/send_enc -sender $address1 -value 1 $auction_contract $(cast sig "bid()")'
-verbosely 'go run ./script/send_enc -sender $address2 -value 2 $auction_contract $(cast sig "bid()")'
+visibly 'go run ./script/send_enc -sender $address1 -value 1 $auction_contract $(cast sig "bid()")'
+visibly 'go run ./script/send_enc -sender $address2 -value 2 $auction_contract $(cast sig "bid()")'
 
 sleep 40
 
 (cd contracts
-	verbosely 'forge script --broadcast -f $ETH_RPC_URL --keystore "$ETH_KEYSTORE/$deployer" --sender $deployer script/CloseAuction.s.sol'
+	visibly 'forge script --broadcast -f $ETH_RPC_URL --keystore "$ETH_KEYSTORE/$deployer" --sender $deployer script/CloseAuction.s.sol'
 )
 
 bash

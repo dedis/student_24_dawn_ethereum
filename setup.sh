58f0f29701cf101c1e5f0f895b7b55cdd40dcad2
Julie Bettens <julie.bettens@epfl.ch>
2024-05-22 16:00:00 +0200

    setup meme

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+jq --version > /dev/null || { echo "Please install jq"; exit 1; }
+
 go install github.com/ethereum/go-ethereum/cmd/geth \
            github.com/ethereum/go-ethereum/cmd/abigen \
            go.dedis.ch/f3b/smc/smccli

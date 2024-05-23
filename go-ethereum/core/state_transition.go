82e831ba7ec1f23244ed6b6dbc759c61d3b72e52
Julie Bettens <julie.bettens@epfl.ch>
2024-05-23 19:06:56 +0200

    clean

--- go-ethereum/core/state_transition.go
+++ go-ethereum/core/state_transition.go
@@ -225,56 +225,6 @@ func (st *StateTransition) buyGas() error {
 	return nil
 }
 
-func (st *StateTransition) encryptedPreCheck() (*ExecutionResult, error) {
-	stNonce := st.state.GetNonce(st.msg.From())
-	if msgNonce := st.msg.Nonce(); stNonce == msgNonce {
-		log.Info(fmt.Sprintf("$ address %v, tx: %d state: %d, used gas: %v, ordering the encrypted tx", st.msg.From().Hex(), msgNonce, stNonce, st.gasUsed()))
-		st.state.SetNonce(st.msg.From(), st.state.GetNonce(vm.AccountRef(st.msg.From()).Address())+1)
-
-		return &ExecutionResult{
-			UsedGas:    st.gasUsed(),
-			Err:        fmt.Errorf("ordering"),
-			ReturnData: []byte(""),
-		}, nil
-
-	} else if stNonce < msgNonce { // same as other tx types
-
-		return nil, fmt.Errorf("%w: address %v, tx: %d state: %d", ErrNonceTooHigh, st.msg.From().Hex(), msgNonce, stNonce)
-
-	} else {
-		// Make sure the sender is an EOA
-		if codeHash := st.state.GetCodeHash(st.msg.From()); codeHash != emptyCodeHash && codeHash != (common.Hash{}) {
-			return nil, fmt.Errorf("%w: address %v, codehash: %s", ErrSenderNoEOA, st.msg.From().Hex(), codeHash)
-		}
-
-		// Make sure that transaction gasFeeCap is greater than the baseFee (post london)
-		if st.evm.ChainConfig().IsLondon(st.evm.Context.BlockNumber) {
-			// Skip the checks if gas fields are zero and baseFee was explicitly disabled (eth_call)
-			if !st.evm.Config.NoBaseFee || st.gasFeeCap.BitLen() > 0 || st.gasTipCap.BitLen() > 0 {
-				if l := st.gasFeeCap.BitLen(); l > 256 {
-					return nil, fmt.Errorf("%w: address %v, maxFeePerGas bit length: %d", ErrFeeCapVeryHigh, st.msg.From().Hex(), l)
-				}
-				if l := st.gasTipCap.BitLen(); l > 256 {
-					return nil, fmt.Errorf("%w: address %v, maxPriorityFeePerGas bit length: %d", ErrTipVeryHigh, st.msg.From().Hex(), l)
-				}
-				if st.gasFeeCap.Cmp(st.gasTipCap) < 0 {
-					return nil, fmt.Errorf("%w: address %v, maxPriorityFeePerGas: %s, maxFeePerGas: %s", ErrTipAboveFeeCap, st.msg.From().Hex(), st.gasTipCap, st.gasFeeCap)
-				}
-				// This will panic if baseFee is nil, but basefee presence is verified
-				// as part of header validation.
-				if st.gasFeeCap.Cmp(st.evm.Context.BaseFee) < 0 {
-					return nil, fmt.Errorf("%w: address %v, maxFeePerGas: %s baseFee: %s", ErrFeeCapTooLow, st.msg.From().Hex(), st.gasFeeCap, st.evm.Context.BaseFee)
-				}
-			}
-		}
-		// buy gas for execution
-		if err := st.buyGas(); err != nil {
-			return nil, err
-		}
-	}
-	return nil, nil
-}
-
 func (st *StateTransition) preCheck() error {
 	// Only check transactions that are not fake
 	if !st.msg.IsFake() {
@@ -296,6 +246,8 @@ func (st *StateTransition) preCheck() error {
 			return fmt.Errorf("%w: address %v, codehash: %s", ErrSenderNoEOA,
 				st.msg.From().Hex(), codeHash)
 		}
+	} else {
+		log.Info("fake message", "msg", st.msg)
 	}
 	// Make sure that transaction gasFeeCap is greater than the baseFee (post london)
 	if st.evm.ChainConfig().IsLondon(st.evm.Context.BlockNumber) {
@@ -348,19 +300,9 @@ func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
 	// 5. there is no overflow when calculating intrinsic gas
 	// 6. caller has enough balance to cover asset transfer for **topmost** call
 
-	// early exit when ordering the encrypted tx
-	if st.msg.Type() == types.EncryptedTxType {
-		execResult, err := st.encryptedPreCheck()
-		if execResult != nil || err != nil {
-			return execResult, err
-		} else {
-			//only execution of encrypted tx follows
-		}
-	} else {
-		// Check clauses 1-3, buy gas if everything is correct
-		if err := st.preCheck(); err != nil {
-			return nil, err
-		}
+	// Check clauses 1-3, buy gas if everything is correct
+	if err := st.preCheck(); err != nil {
+		return nil, err
 	}
 
 	if st.evm.Config.Debug {
@@ -405,13 +347,7 @@ func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
 		ret, _, st.gas, vmerr = st.evm.Create(sender, st.data, st.gas, st.value)
 	} else {
 		// Increment the nonce for the next transaction
-		if st.msg.Type() != types.EncryptedTxType {
-			st.state.SetNonce(msg.From(), st.state.GetNonce(sender.Address())+1)
-		}
-		if st.msg.Type() == types.EncryptedTxType {
-			// st.evm.Context.Coinbase = common.BigToAddress(big.NewInt(0))
-			log.Info(fmt.Sprintf("execution of encrypted tx, coinbase: %v", st.evm.Context.Coinbase))
-		}
+		st.state.SetNonce(msg.From(), st.state.GetNonce(sender.Address())+1)
 		ret, st.gas, vmerr = st.evm.Call(sender, st.to(), st.data, st.gas, st.value)
 	}
 
